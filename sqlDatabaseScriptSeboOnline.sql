4934b8bdc0bcb41024e7147c188b827ba5eadbfc
Paulo Silveira <49129267+asjr-p@users.noreply.github.com>
2023-11-15 16:01:51 -0300

    Add files via upload

--- sqlDatabaseScriptSeboOnline.sql
+++ sqlDatabaseScriptSeboOnline.sql
@@ -1,23 +1,137 @@
-CREATE TABLE users (
-  idusers int NOT NULL AUTO_INCREMENT,
-  name varchar(45) DEFAULT NULL,
-  email varchar(45) DEFAULT NULL,
-  password varchar(256) DEFAULT NULL,
-  usertype varchar(45) DEFAULT NULL,
-  status varchar(45) DEFAULT NULL,
-  PRIMARY KEY (idusers)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sebo
+-- ------------------------------------------------------
+-- Server version	8.0.35
 
-INSERT INTO users VALUES (1,'root','root@email.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','admin','ativo'),(3,'mudei o nome paulo','novo@email.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','vendedor','ativo'),(4,'test','novo@email.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','vendedor','ativo'),(5,'mudei o teste','testei@email.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','comprador','deactivated'),(6,'mudei o teste','testei@email.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','comprador','ativo'),(7,'testei','paulopaulo@email.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','vendedor','ativo');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE items (
-  iditems int NOT NULL AUTO_INCREMENT,
-  titulo varchar(45) DEFAULT NULL,
-  autor varchar(45) DEFAULT NULL,
-  categoria varchar(45) DEFAULT NULL,
-  preco int DEFAULT NULL,
-  status varchar(45) DEFAULT NULL,
-  PRIMARY KEY (iditems)
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `idcategories` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `status` varchar(45) NOT NULL,
+  PRIMARY KEY (`idcategories`,`name`,`status`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO items VALUES (1,'AI at the Edge','Daniel Situnayake e Jenny Plunkett','Livro',322,'deactivated'),(2,'AI at the Edge','Daniel Situnayake e Jenny Plunkett','Livro',500,'ATIVO'),(3,'AI at the Edge','Daniel Situnayake e Jenny Plunkett','Livro',500,'INATIVO'),(4,'Livro teste','um bom autor','Livro',1000,'ATIVO'),(5,'Livro teste parte 2','um bom autor','Livro',1000,'ATIVO'),(6,'Diario de um banana','um bom autor banana','Livro',10000,'ATIVO');
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'gibis','INATIVO'),(2,'livro','ativo'),(3,'revistas','ativo'),(4,'periodicos','ativo'),(5,'jornais','ativo'),(6,'tech','INATIVO');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `items` (
+  `iditems` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(45) DEFAULT NULL,
+  `autor` varchar(45) DEFAULT NULL,
+  `categoria` varchar(45) DEFAULT NULL,
+  `preco` int DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `isbn` int DEFAULT NULL,
+  PRIMARY KEY (`iditems`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+INSERT INTO `items` VALUES (1,'AI at the Edge','Daniel Situnayake e Jenny Plunkett','Livro',322,'deactivated',NULL),(2,'AI at the Edge','Daniel Situnayake e Jenny Plunkett','Livro',500,'INATIVO',NULL),(3,'AI at the Edge','Daniel Situnayake e Jenny Plunkett','Livro',500,'INATIVO',NULL),(4,'Livro teste','um bom autor','Livro',1000,'ATIVO',NULL),(5,'Livro teste parte 2','um bom autor','Livro',1000,'ATIVO',NULL),(6,'Diario de um banana','um bom autor banana','Livro',10000,'ATIVO',NULL),(7,'Diario de um banana 2.0','um bom autor banana','livro',10000,'ATIVO',NULL),(8,'validando atualização','um bom autor validando','livro',25,'ATIVO',NULL),(9,'Livro ISBN','um bom dev validando','livro',1000,'ATIVO',1111111),(10,'Livro validado','um bom dev validando','livro',1000,'ATIVO',1111341);
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transactions` (
+  `idtransactions` int NOT NULL AUTO_INCREMENT,
+  `idbuyer` int NOT NULL,
+  `idseller` int NOT NULL,
+  `iditem` int NOT NULL,
+  `value` float NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`idtransactions`,`idbuyer`,`idseller`,`iditem`,`value`,`date`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transactions`
+--
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+INSERT INTO `transactions` VALUES (1,9,1,7,10000,'2023-11-13'),(2,9,1,7,10000,'2023-11-13'),(3,9,1,7,10000,'2023-11-13'),(4,9,1,7,10000,'2023-11-13'),(5,9,1,8,25,'2023-11-13'),(6,9,1,8,25,'2023-11-13');
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `idusers` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `password` varchar(256) DEFAULT NULL,
+  `usertype` varchar(45) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idusers`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'root','root@email.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','admin','ativo'),(3,'mudei o nome paulo','novo@email.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','vendedor','ativo'),(4,'test','novo@email.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','vendedor','deactivated'),(5,'mudei o teste','testei@email.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','comprador','ativo'),(6,'mudei o teste','testei@email.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','comprador','ativo'),(7,'testei','paulopaulo@email.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','vendedor','ativo'),(8,'admin','admin@email.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','admin','ativo'),(9,'buyer','buyer@email.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','comprador','ativo'),(10,'comprador','comprador@email.com','481f6cc0511143ccdd7e2d1b1b94faf0a700a8b49cd13922a70b5ae28acaa8c5','comprador','ativo');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-15 12:02:34
